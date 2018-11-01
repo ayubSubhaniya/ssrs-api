e2fee61f002aa481cb81c2bac172a4ea81f77be5
Ayub Subhaniya <ayub.subhaniya@gmail.com>
2018-11-01 17:02:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,17 +73,17 @@ const accessLogStream = rfs('access.log', {
 /* CONNECTING TO MongoDB */
 
 /* Local Database */
-/*const DB_HOST = process.env.DB_HOST;
+const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_COLLECTION_NAME = process.env.DB_COLLECTION_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 
-const dbURI = 'mongodb://ssrsDaiict:ssrsDaiict123@localhost:27017/ssrs-daiict';*/
+const dbURI = 'mongodb://ssrsDaiict:ssrsDaiict123@localhost:27017/ssrs-daiict';
 
 
 /* Online Database */
-const dbURI = process.env.DB_URI;
+//const dbURI = process.env.DB_URI;
 
 db.connect(dbURI)
     .then(
