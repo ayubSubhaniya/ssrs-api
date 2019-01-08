fe98ad451011a617fbd9fe99f9aaa6cc072a638e
Jaimin <jaiminchaudhary42@gmail.com>
2019-01-09 00:59:02 +0530

    added check for parameter/service change in current cart(before placed)

--- controllers/order.js
+++ controllers/order.js
@@ -79,6 +79,7 @@ const calculateParameterCost = async (parameters, requiredUnits, availableParame
 
 const recalculateOrderCost = async (order, user) => {
     const service = await Service.findById(order.service);
+    let message = "your order with service : " + order.serviceName + "is removed due to service/parameter description change.Please try again.";
 
     if (!service) {
         await Order.findByIdAndRemove(order._id);
@@ -90,13 +91,87 @@ const recalculateOrderCost = async (order, user) => {
         });
 
         /* Add notification here*/
-        let message = "your order with service : " + order.serviceName + "is removed due to service/parameter description change.Please try again.";
         const notification = generateCurreptedOrderRemovalNotification(order.requestedBy, systemAdmin, message);
         await notification.save();
-        
         return null;
     }
 
+    const prmtr = order.parameters;
+    const requiredUnits = order.unitsRequested;
+
+    const specialServiceValidation = !service.isSpecialService || service.specialServiceUsers.includes(user.daiictId);
+    const useServiceValidation = (!user.userInfo.user_batch || (service.allowedBatches.includes('*') || service.allowedBatches.includes(user.userInfo.user_batch))) &&
+        (!user.userInfo.user_programme || (service.allowedProgrammes.includes('*') || service.allowedProgrammes.includes(user.userInfo.user_programme))) &&
+        (!user.userInfo.user_status || (service.allowedUserStatus.includes('*') || service.allowedUserStatus.includes(user.userInfo.user_status)));
+
+    if (!specialServiceValidation || !useServiceValidation || !service.isActive || requiredUnits > service.maxUnits || requiredUnits <= 0) {
+        await Order.findByIdAndRemove(order._id);
+
+        await Cart.findByIdAndUpdate(order.cartId, {
+            'pull': {
+                'orders': order._id
+            }
+        });
+
+        /* Add notification here*/
+        const notification = generateCurreptedOrderRemovalNotification(order.requestedBy, systemAdmin, message);
+        await notification.save();
+        return null;
+    }
+
+    let availableParameters = service.availableParameters;
+
+    if (availableParameters) {
+        availableParameters = convertToStringArray(availableParameters);
+        for (let i = 0; i < prmtr.length; i++) {
+            const ith_parameter = await Parameter.findById(prmtr[i]);
+            let parameterId;
+            if (ith_parameter._id) {
+                parameterId = ith_parameter._id;
+            } else {
+                parameterId = ith_parameter;
+            }
+            const parameter = await Parameter.findById(parameterId);
+            if (!parameter || !parameter.isActive || !availableParameters.includes(parameterId.toString())) {
+                await Order.findByIdAndRemove(order._id);
+                await Cart.findByIdAndUpdate(order.cartId, {
+                    'pull': {
+                        'orders': order._id
+                    }
+                });
+
+                /* Add notification here*/
+                const notification = generateCurreptedOrderRemovalNotification(order.requestedBy, systemAdmin, message);
+                await notification.save();
+                return null;
+            }
+        }
+    } else {
+        for (let i = 0; i < prmtr.length; i++) {
+            const ith_parameter = await Parameter.findById(prmtr[i]);
+            let parameterId;
+            if (ith_parameter._id) {
+                parameterId = ith_parameter._id;
+            } else {
+                parameterId = ith_parameter;
+            }
+            const parameter = await Parameter.findById(parameterId);
+            if (!parameter || !parameter.isActive) {
+                await Order.findByIdAndRemove(order._id);
+                await Cart.findByIdAndUpdate(order.cartId, {
+                    'pull': {
+                        'orders': order._id
+                    }
+                });
+
+                /* Add notification here*/
+                const notification = generateCurreptedOrderRemovalNotification(order.requestedBy, systemAdmin, message);
+                await notification.save();
+                return null;
+            }    
+        }
+    }
+    
     order.parameterCost = await calculateParameterCost(order.parameters, order.unitsRequested);
     order.serviceCost = await calculateServiceCost(service, order.unitsRequested, user);
     order.totalCost = 0;
