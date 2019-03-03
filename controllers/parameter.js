a5efa27d00d2f5fbe3ef99112a5662fdccb7cc8f
Ayub Subhaniya <ayub.subhaniya@sprinklr.com>
2019-03-04 02:38:32 +0530

    removed console.log

--- controllers/parameter.js
+++ controllers/parameter.js
@@ -10,11 +10,8 @@ const { filterResourceData } = require('../helpers/controllerHelpers');
 const { generateCurreptedOrderRemovalNotification } = require('../helpers/notificationHelper');
 
 const removeDeletedParameterFromService = async (parameterId) => {
-    console.log("here");
     const services = await Service.find({ availableParameters: parameterId });
-    console.log(services.length);
     for (let i=0;i<services.length;i++){
-        console.log(services[i].name);
         await Service.findByIdAndUpdate(services[i]._id,{
             'pull':{
                 'availableParameters':parameterId
