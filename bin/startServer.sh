e5c96d0b502889d7f3b229c7ac3de570ddaa5358
harshvasoya008 <003harsh008@gmail.com>
2019-05-15 12:48:12 +0530

    deploy script updated

--- bin/startServer.sh
+++ bin/startServer.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-source ./bin/envVar.sh
-nodemon
+
+# Here '3' is the number of CPUs to be used.
+# use 'max'   - for all using all CPUs
+#     '-1'    - for all CPUs minus 1    
+pm2 start ../app.js -i 3
