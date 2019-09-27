01e244235a60085d41f3fd4f326754b277d04362
Harsh Vasoya <003harsh008@gmail.com>
2019-09-27 17:07:05 +0530

    Adding pm2 logging file

--- bin/startServer.bat
+++ bin/startServer.bat
@@ -5,4 +5,4 @@ pm2 stop all
 :: Here last word of command is the number of CPUs to be used.
 :: use 'max'   - for all using all CPUs
 ::     '-1'    - for all CPUs minus 1    
-pm2 start app.js -i 1
+pm2 start app.js --time --log logs/pm2_logs.log -i 1
